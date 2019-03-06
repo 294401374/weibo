69810b17a13315dd50065f7800259e07820a9e3f
vagrant <vagrant@homestead>
2019-03-06 07:54:25 +0000

    修改default页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Weibo App') - Laravel新手入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    <div class="container">
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
           <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
@@ -17,5 +19,5 @@
     <div class="container">
       @yield('content')
     </div>
-</body>
+  </body>
 </html>

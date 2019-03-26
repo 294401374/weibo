c11d3e9d0d38f4764a1c389414e883f76ed1ec30
小剑 <294401374@qq.com>
2019-03-26 14:53:20 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 获取头像方法
+     * @return [type] [description]
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }

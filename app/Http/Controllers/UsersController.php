dc113a7f6c26901ba723a92d67d0c5b29566fdb6
小剑 <294401374@qq.com>
2019-03-28 13:58:27 +0000

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,14 +7,25 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
+    //用户注册表单页面
     public function create()
     {
     	return view('users.create');
     }
 
+    //用户个人信息展示页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        return;
     }
 }

24db822e3af448c49c5b2861c44c24ae4c2a8bee
小剑 <294401374@qq.com>
2019-03-31 05:25:19 +0000

    测试heroku的配置

--- app/helpers.php
+++ app/helpers.php
@@ -2,22 +2,24 @@
 // 根据环境不同配置数据库
 function get_db_config()
 {
-    if (getenv('IS_IN_HEROKU')){
-        $url = parse_url(getenv('DATABASE_URL'));
+    var_dump(getenv('IS_IN_HEROKU'));
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
         return $db_config = [
             'connection' => 'pgsql',
-            'host' => $url["host"],
+            'host'  => $url["host"],
             'database'  => substr($url["path"], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
         ];
     } else {
         return $db_config = [
-            'connection'    => env('DB_CONNECTION', 'mysql'),
-            'host'          => env('DB_HOST', '127.0.0.1'),
-            'database'      => env('DB_DATABASE', 'forge'),
-            'username'      => env('DB_USERNAME', 'forge'),
-            'password'      => env('DB_PASSWORD', ''),
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
 }

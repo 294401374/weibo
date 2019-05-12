8d3325b89df4988f8efbd0917fb6c2c234bd8c7a
小剑 <294401374@qq.com>
2019-05-12 04:09:29 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()
+                                        ->paginate(10);
+        }
     	// return '主页';
-    	return view('static_pages/home');
+    	return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
@@ -17,7 +23,7 @@ class StaticPagesController extends Controller
     	// return '帮助页';
     	return view('static_pages/help');
     }
-    
+
     public function about()
     {
     	// return '关于页';
